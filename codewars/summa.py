d05b71c29d94deac0a035d3dfc13af62c64fba87
Kirill Talatynnik <ktalatynnik@mail.ru>
2023-12-07 16:23:41 +0300

    Summa

--- codewars/summa.py
+++ codewars/summa.py
@@ -10,3 +10,12 @@ def sum_of_sums(n):
 
 assert sum_of_sums(3) == 55
 assert sum_of_sums(5) == 630
+
+from collections import Counter
+def find_maximum(nums):
+    return Counter(nums).most_common(1)[0][0]
+
+
+list1 = [1, 3, 6, 2, 12, 7, 9, 4, 3, 1, 12, 4, 5, 2, 4, 4]
+maximux = find_maximum(list1)
+print('Maximum', maximux)
